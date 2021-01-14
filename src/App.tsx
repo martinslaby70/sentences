36ed766ef53f7375401196327af54dd898764b13
Martin Slabý <martin.slaby29@seznam.cz>
2021-01-14 14:02:21 +0100

    lazy-loading component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { LazyLoadComponent } from 'react-lazy-load-image-component';
 
 //redux
 import { store } from './redux/store';
@@ -14,7 +15,11 @@ const App = () => {
   return(
     <Provider store={store}>
       <FormContainer />
-      <Sentenses />    
+
+      <LazyLoadComponent>
+        <Sentenses />   
+      </LazyLoadComponent> 
+
     </Provider>
   )
 }
